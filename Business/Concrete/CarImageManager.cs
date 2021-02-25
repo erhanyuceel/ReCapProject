0c7fef974069c61b01307c28b3595040016466a8
erhanyuceel <erhanyuceel@gmail.com>
2021-02-25 22:34:35 +0300

    #ödevin bir kısmı yapıldı

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        [ValidationAspect(typeof(CarImageValidator))]
+        public IResult Add(CarImage carImage)
+        {
+            var addedCarImage = CreatedFile(carImage).Data;
+            _carImageDal.Add(addedCarImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

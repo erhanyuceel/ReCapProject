7032ad9341524adfdab28e14c0bb280398b946e6
Erhan YÜCEEL <erhanyuceel@gmail.com>
2021-03-14 22:49:14 +0300

    #front-end bağlantısı yapıldı

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -67,6 +68,7 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(1000);
             var result = _carService.GetAll();
             if (result.Success)
             {
